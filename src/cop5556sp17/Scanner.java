cbc3bfc9f20eae070319c1e7df71626c7c78a6bb
Suraj Kota <k.suraj1993@gmail.com>
2017-02-01 09:18:37 -0500

    Scanner V1
    Scanner for programming language with following lexical Structure:
    
    comment ::=   /*   NOT(*/)*  */
    token ::= ident  | keyword | frame_op_keyword | filter_op_keyword |
    image_op_keyword | boolean_literal
    | int_literal  | separator  | operator
    ident ::= ident_start  ident_part*    (but not reserved)
    ident_start ::=  A .. Z | a .. z | $ | _
    ident_part ::= ident_start | ( 0 .. 9 )
    int_literal ::= 0  |  (1..9) (0..9)*
    keyword ::= integer | boolean | image | url | file | frame | while | if
    | sleep | screenheight | screenwidth
    filter_op_keyword ∷= gray | convolve | blur | scale
    image_op_keyword ∷= width | height
    frame_op_keyword ∷= xloc | yloc | hide | show | move
    boolean_literal ::= true | false
    separator ::=  ;  | ,  |  (  |  )  | { | }
    operator ::=    |  | &  |  ==  | !=  | < |  > | <= | >= | +  |  -  |  *
    |  /   |  % | !  | -> |  |-> | <-

---
+++ src/cop5556sp17/Scanner.java
@@ -0,0 +1,464 @@
+package cop5556sp17;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Scanner {
+	/**
+	 * Kind enum
+	 */
+	
+	public static enum Kind {
+		IDENT(""), INT_LIT(""), KW_INTEGER("integer"), KW_BOOLEAN("boolean"), 
+		KW_IMAGE("image"), KW_URL("url"), KW_FILE("file"), KW_FRAME("frame"), 
+		KW_WHILE("while"), KW_IF("if"), KW_TRUE("true"), KW_FALSE("false"), 
+		SEMI(";"), COMMA(","), LPAREN("("), RPAREN(")"), LBRACE("{"), 
+		RBRACE("}"), ARROW("->"), BARARROW("|->"), OR("|"), AND("&"), 
+		EQUAL("=="), NOTEQUAL("!="), LT("<"), GT(">"), LE("<="), GE(">="), 
+		PLUS("+"), MINUS("-"), TIMES("*"), DIV("/"), MOD("%"), NOT("!"), 
+		ASSIGN("<-"), OP_BLUR("blur"), OP_GRAY("gray"), OP_CONVOLVE("convolve"), 
+		KW_SCREENHEIGHT("screenheight"), KW_SCREENWIDTH("screenwidth"), 
+		OP_WIDTH("width"), OP_HEIGHT("height"), KW_XLOC("xloc"), KW_YLOC("yloc"), 
+		KW_HIDE("hide"), KW_SHOW("show"), KW_MOVE("move"), OP_SLEEP("sleep"), 
+		KW_SCALE("scale"), EOF("eof");
+
+		Kind(String text) {
+			this.text = text;
+		}
+
+		final String text;
+
+		String getText() {
+			return text;
+		}
+	}
+/**
+ * Thrown by Scanner when an illegal character is encountered
+ */
+	@SuppressWarnings("serial")
+	public static class IllegalCharException extends Exception {
+		public IllegalCharException(String message) {
+			super(message);
+		}
+	}
+	
+	/**
+	 * Thrown by Scanner when an int literal is not a value that can be represented by an int.
+	 */
+	@SuppressWarnings("serial")
+	public static class IllegalNumberException extends Exception {
+	public IllegalNumberException(String message){
+		super(message);
+		}
+	}
+	
+
+	/**
+	 * Holds the line and position in the line of a token.
+	 */
+	static class LinePos {
+		public final int line;
+		public final int posInLine;
+		
+		public LinePos(int line, int posInLine) {
+			super();
+			this.line = line;
+			this.posInLine = posInLine;
+		}
+
+		@Override
+		public String toString() {
+			return "LinePos [line=" + line + ", posInLine=" + posInLine + "]";
+		}
+	}
+		
+
+	
+
+	public class Token {
+		public final Kind kind;
+		public final int pos;  //position in input array
+		public final int length;  
+
+		//returns the text of this Token
+		public String getText() {
+			//TODO IMPLEMENT THIS
+			if(this.length>0){
+			return chars.substring(this.pos, this.pos+this.length);
+			}
+			else if(this.length == 0){
+				return "EOF";
+			}
+			return null;
+		}
+		
+		//returns a LinePos object representing the line and column of this Token
+		LinePos getLinePos(){
+			//TODO IMPLEMENT THIS
+			int line_num = Collections.binarySearch(line_numbers, this.pos);
+			if(line_num<0){
+				line_num=Math.abs(line_num);
+				line_num-=2;
+			}
+			return new LinePos(line_num, this.pos-line_numbers.get(line_num));
+			//return null;
+		}
+
+		Token(Kind kind, int pos, int length) {
+			this.kind = kind;
+			this.pos = pos;
+			this.length = length;
+		}
+
+		/** 
+		 * Precondition:  kind = Kind.INT_LIT,  the text can be represented with a Java int.
+		 * Note that the validity of the input should have been checked when the Token was created.
+		 * So the exception should never be thrown.
+		 * 
+		 * @return  int value of this token, which should represent an INT_LIT
+		 * @throws NumberFormatException
+		 * @throws IllegalNumberException 
+		 */
+		public int intVal() throws NumberFormatException, IllegalNumberException{
+			//TODO IMPLEMENT THIS
+			int tokenvalue = 0;
+			if(this.kind==Kind.INT_LIT){
+			
+				try{
+					tokenvalue = Integer.parseInt(chars.substring(this.pos, this.pos+this.length));
+					
+				}catch(Exception E){
+					throw new IllegalNumberException("Number: "+chars.substring(this.pos, this.pos+this.length)+" out of int bounds at pos "+this.pos);
+				}
+			}
+			return tokenvalue;
+		}
+		
+	}
+
+	 
+
+
+	Scanner(String chars) {
+		this.chars = chars;
+		tokens = new ArrayList<Token>();
+		line_numbers = new ArrayList<>();
+		line_numbers.add(0);
+		tokenNum=0;
+		pos=0;
+
+	}
+
+	public static enum State {
+		START,IN_DIGIT,IN_IDENT,AFTER_EQ,AFTER_NOT,AFTER_MINUS,AFTER_GT,
+		AFTER_OR,AFTER_LT,AFTER_DIV,COMMENT_START;
+	}
+
+	public int skipWhiteSpace(){
+		int tch;
+		int tlength=chars.length();
+		while(pos<tlength && Character.isWhitespace(tch = chars.charAt(pos))){
+			if(tch=='\n'){
+				line_numbers.add(pos+1);//Change- Not End of line(\n) position but where the new line starts
+			}
+			pos++;
+		}
+		return pos;
+	}
+	/**
+	 * Initializes Scanner object by traversing chars and adding tokens to tokens list.
+	 * 
+	 * @return this scanner
+	 * @throws IllegalCharException
+	 * @throws IllegalNumberException
+	 */
+	
+	public Scanner scan() throws IllegalCharException, IllegalNumberException {
+		pos=0; 
+		//TODO IMPLEMENT THIS!!!!
+		int length=chars.length();
+		int startPos=0;
+		int ch=-10;
+		State state=State.START;
+		
+		while(pos<=length){
+			ch = pos < length ? chars.charAt(pos) : -1;
+        switch (state) {
+            case START: {
+            	pos=skipWhiteSpace();
+            	ch = pos < length ? chars.charAt(pos) : -1;
+            	startPos=pos;
+            	switch(ch){
+            	case -1: {tokens.add(new Token(Kind.EOF, pos, 0)); pos++;}  break;
+                case '+': {tokens.add(new Token(Kind.PLUS, startPos, 1));pos++;} break;
+                case '*': {tokens.add(new Token(Kind.TIMES, startPos, 1));pos++;} break;
+                case '%': {tokens.add(new Token(Kind.MOD, startPos, 1));pos++;} break;
+                case '&': {tokens.add(new Token(Kind.AND, startPos, 1));pos++;} break;
+                case ';': {tokens.add(new Token(Kind.SEMI,startPos, 1));pos++;}break;
+                case ',': {tokens.add(new Token(Kind.COMMA,startPos, 1));pos++;}break;
+                case '(': {tokens.add(new Token(Kind.LPAREN,startPos, 1));pos++;}break;
+                case ')': {tokens.add(new Token(Kind.RPAREN,startPos, 1));pos++;}break;
+                case '{': {tokens.add(new Token(Kind.LBRACE,startPos, 1));pos++;}break;
+                case '}': {tokens.add(new Token(Kind.RBRACE,startPos, 1));pos++;}break;
+                case '0': {tokens.add(new Token(Kind.INT_LIT,startPos, 1));pos++;}break;
+                
+                case '=': {state = State.AFTER_EQ;pos++;}break;
+                case '!': {state = State.AFTER_NOT;pos++;}break;
+                case '-': {state = State.AFTER_MINUS;pos++;}break;
+                case '>': {state = State.AFTER_GT;pos++;}break;
+                case '|': {state = State.AFTER_OR;pos++;}break;
+                case '<': {state = State.AFTER_LT;pos++;}break;
+                case '/': {state = State.AFTER_DIV;pos++;}break;
+                
+                default: {
+                    if (Character.isDigit(ch)) {state = State.IN_DIGIT;pos++;} 
+                    else if (Character.isJavaIdentifierStart(ch)) {
+                         state = State.IN_IDENT;pos++;
+                     } 
+                     else {throw new IllegalCharException(
+                                "illegal char " +ch+" at pos "+pos);
+                     }
+                  }
+            	}
+            }  break;
+            case IN_DIGIT: {
+            	ch = pos < length ? chars.charAt(pos) : -1;
+            	if (Character.isDigit(ch)) {
+            		pos++;
+            	}
+            	else{
+            		
+            		try{
+            			Integer.parseInt(chars.substring(startPos, pos));
+            		}catch(Exception E){
+            			throw new IllegalNumberException("Number: "+chars.substring(startPos, pos)+" out of int bounds at pos"+pos);
+            		}
+            		
+            		tokens.add(new Token(Kind.INT_LIT,startPos, pos-startPos));
+            		state = State.START;
+            	}
+            }  break;
+            case IN_IDENT: {
+            	
+            	ch = pos < length ? chars.charAt(pos) : -1;
+                if (Character.isJavaIdentifierPart(ch)) {
+                    pos++;
+              } else {
+            	  boolean isnotkeyword = true;
+            	  String test_thisident=chars.substring(startPos, pos);
+            	  for(Kind type: Kind.values()){
+            		  if(type.getText().equals(test_thisident)){
+            			  tokens.add(new Token(type, startPos, pos - startPos));
+            			  isnotkeyword=false;
+            			  break;
+            		  }
+            	  }
+            	  if(isnotkeyword){
+            		  tokens.add(new Token(Kind.IDENT, startPos, pos - startPos));
+            	  }
+            	  state = State.START;
+              }
+
+            }  break;
+            case AFTER_EQ: {
+            	
+            	ch = pos < length ? chars.charAt(pos) : -1;
+            	if(ch=='='){
+            		tokens.add(new Token(Kind.EQUAL, startPos, 2));
+            		pos++;
+            		state = State.START;
+            	}
+            	else{
+            		throw new IllegalCharException("illegal char " +ch+" at pos "+pos);
+            	}
+            }  break;
+            case AFTER_NOT: {
+            	
+            	ch = pos < length ? chars.charAt(pos) : -1;
+            	if(ch=='='){
+            		tokens.add(new Token(Kind.NOTEQUAL, startPos, 2));
+            		pos++;
+            		state = State.START;
+            	}
+            	else{
+            		tokens.add(new Token(Kind.NOT, startPos, 1));
+            		state=State.START;
+            	}
+            }  break;
+            case AFTER_MINUS: {
+            	
+            	ch = pos < length ? chars.charAt(pos) : -1;
+            	if(ch=='>'){
+            		tokens.add(new Token(Kind.ARROW, startPos, 2));
+            		pos++;
+            		state = State.START;
+            	}
+            	else{
+            		tokens.add(new Token(Kind.MINUS, startPos, 1));
+            		state=State.START;
+            	}
+            }  break;
+            case AFTER_GT: {
+            	
+            	ch = pos < length ? chars.charAt(pos) : -1;
+            	if(ch=='='){
+            		tokens.add(new Token(Kind.GE, startPos, 2));
+            		pos++;
+            		state = State.START;
+            	}
+            	else{
+            		tokens.add(new Token(Kind.GT, startPos, 1));
+            		state=State.START;
+            	}
+            }  break;
+            case AFTER_OR: {
+            	
+            	ch = pos < length ? chars.charAt(pos) : -1;
+            	if(ch=='-'){
+            		pos++;
+            		
+                	ch = pos < length ? chars.charAt(pos) : -1;
+                	if(ch == '>'){
+                		tokens.add(new Token(Kind.BARARROW, startPos, 3));
+                		pos++;
+                		state = State.START;
+                	}
+                	else{
+                		throw new IllegalCharException("illegal char " +ch+" at pos "+pos);
+                	}
+            	}
+            	else{
+            		tokens.add(new Token(Kind.OR, startPos, 1));
+            		state=State.START;
+            	}
+            }  break;
+            case AFTER_LT: {
+            	
+            	ch = pos < length ? chars.charAt(pos) : -1;
+            	if(ch == '-'){
+            		tokens.add(new Token(Kind.ASSIGN, startPos, 2));
+            		pos++;
+            		state = State.START;
+            	}
+            	else if(ch == '='){
+            		tokens.add(new Token(Kind.LE, startPos, 2));
+            		pos++;
+            		state=State.START;
+            	}
+            	else{
+            		tokens.add(new Token(Kind.LT, startPos, 1));
+            		state=State.START;
+            	}
+            }  break;
+            case AFTER_DIV: {
+            	
+            	ch = pos < length ? chars.charAt(pos) : -1;
+            	if(ch=='*'){
+            		pos++;
+            		state = State.COMMENT_START;
+            	}
+            	else{
+            		tokens.add(new Token(Kind.DIV, startPos, 1));
+            		state=State.START;
+            	}
+            }  break;
+            case COMMENT_START: {
+            	pos=skipWhiteSpace();
+            	ch = pos < length ? chars.charAt(pos) : -1;
+            	if(ch=='*'){
+            		pos++;            		
+                	ch = pos < length ? chars.charAt(pos) : -1;
+                	if(ch=='/'){
+                		pos++;
+                		state = State.START;
+                	}
+                	else if(ch == -1) {									/*required or not*/
+                		//TODO throw exception
+                		throw new IllegalCharException("unclosed comment at EOF  " +ch+" at pos "+pos);
+                		//tokens.add(new Token(Kind.EOF, pos, 0)); pos++;
+                		
+                	}
+                	else{
+                		//pos++;		//Skipping comment char
+                	}
+            	}
+            	else if(ch == -1) {
+            		//TODO throw exception
+            		throw new IllegalCharException("unclosed comment at EOF " +ch+" at pos "+pos);
+            		//tokens.add(new Token(Kind.EOF, pos, 0)); pos++;
+            	}
+            	else{
+            		pos++;			//Skipping comment char
+            	}
+            }  break;
+            default:  assert false;
+        }// switch(state)
+    } // while
+    //return this;
+		if(ch!=-1)
+		{tokens.add(new Token(Kind.EOF,pos,0));}
+		return this;  
+	}
+/*
+*/
+	final ArrayList<Token> tokens;
+	final ArrayList<Integer> line_numbers;
+	final String chars;
+	int tokenNum;
+	int pos;
+	/*
+	 * Return the next token in the token list and update the state so that
+	 * the next call will return the Token..  
+	 */
+	public Token nextToken() {
+		if (tokenNum >= tokens.size())
+			return null;
+		return tokens.get(tokenNum++);
+	}
+	
+	
+	/*
+	 * Return the next token in the token list without updating the state.
+	 * (So the following call to next will return the same token.)
+	 */public Token peek() {
+	    if (tokenNum >= tokens.size())
+	        return null;
+	    return tokens.get(tokenNum);
+	}
+	/*
+	 * Return the next token in the token list without updating the state.
+	 * (So the following call to next will return the same token.)
+	 
+	public Token peek(){
+		if (tokenNum >= tokens.size())
+			return null;
+		return tokens.get(tokenNum+1);		
+	}*/
+
+	
+
+	/**
+	 * Returns a LinePos object containing the line and position in line of the 
+	 * given token.  
+	 * 
+	 * Line numbers start counting at 0
+	 * @param t
+	 * @return
+	 */
+	public LinePos getLinePos(Token t) {
+		//TODO IMPLEMENT THIS
+		return t.getLinePos();
+		//return null;
+	}
+	
+	public void printall(){
+		for(int i=0;i<tokens.size();i++){
+			Token t = tokens.get(i);
+			System.out.println("Kind: " + t.kind.getText() + " token: "+t.getText() + " pos: "+t.pos + " len: "+t.length);
+			System.out.println(t.getLinePos().toString());
+			//System.out.println(" Line: "+t.getLinePos().line+" posinLine: "+t.getLinePos().posInLine);
+		}
+	}
+
+
+}
---
+++ src/cop5556sp17/ScannerTest.java
@@ -0,0 +1,155 @@
+package cop5556sp17;
+
+import static cop5556sp17.Scanner.Kind.SEMI;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import cop5556sp17.Scanner.IllegalCharException;
+import cop5556sp17.Scanner.IllegalNumberException;
+
+public class ScannerTest {
+
+	@Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+
+	
+	@Test
+	public void testEmpty() throws IllegalCharException, IllegalNumberException {
+		String input = "";
+		Scanner scanner = new Scanner(input);
+		scanner.scan();
+	}
+
+	@Test
+	public void testSemiConcat() throws IllegalCharException, IllegalNumberException {
+		//input string
+		String input = ";;;";
+		//create and initialize the scanner
+		Scanner scanner = new Scanner(input);
+		scanner.scan();
+		//get the first token and check its kind, position, and contents
+		Scanner.Token token = scanner.nextToken();
+		assertEquals(SEMI, token.kind);
+		assertEquals(0, token.pos);
+		String text = SEMI.getText();
+		assertEquals(text.length(), token.length);
+		assertEquals(text, token.getText());
+		//get the next token and check its kind, position, and contents
+		Scanner.Token token1 = scanner.nextToken();
+		assertEquals(SEMI, token1.kind);
+		assertEquals(1, token1.pos);
+		assertEquals(text.length(), token1.length);
+		assertEquals(text, token1.getText());
+		Scanner.Token token2 = scanner.nextToken();
+		assertEquals(SEMI, token2.kind);
+		assertEquals(2, token2.pos);
+		assertEquals(text.length(), token2.length);
+		assertEquals(text, token2.getText());
+		//check that the scanner has inserted an EOF token at the end
+		Scanner.Token token3 = scanner.nextToken();
+		assertEquals(Scanner.Kind.EOF,token3.kind);
+	}
+	
+	
+	/**
+	 * This test illustrates how to check that the Scanner detects errors properly. 
+	 * In this test, the input contains an int literal with a value that exceeds the range of an int.
+	 * The scanner should detect this and throw and IllegalNumberException.
+	 * 
+	 * @throws IllegalCharException
+	 * @throws IllegalNumberException
+	 */
+	@Test
+	public void testIntOverflowError() throws IllegalCharException, IllegalNumberException{
+		//String input = "99999999999999999";
+		String input = "99999";
+		Scanner scanner = new Scanner(input);
+		//thrown.expect(IllegalNumberException.class);
+		scanner.scan();		
+		Scanner.Token token1 = scanner.nextToken();
+		assertEquals(Scanner.Kind.INT_LIT, token1.kind);
+		assertEquals(0, token1.pos);
+		String text = token1.getText();
+		assertEquals(text.length(), token1.length);
+		assertEquals(99999, token1.intVal());
+		Scanner.LinePos l1 = token1.getLinePos();
+		assertEquals(0,l1.line);
+		assertEquals(0,l1.posInLine);
+	}
+
+//TODO  more tests
+	@Test
+	public void testWhiteSpace() throws IllegalCharException, IllegalNumberException{
+		String input = "sleep\n   def";
+		Scanner scanner = new Scanner(input);
+//		thrown.expect(IllegalNumberException.class);
+		scanner.scan();		
+		Scanner.Token token = scanner.nextToken();
+		assertEquals(Scanner.Kind.OP_SLEEP, token.kind);
+		assertEquals(0, token.pos);
+		String text = Scanner.Kind.OP_SLEEP.getText();
+		assertEquals(text.length(), token.length);
+		assertEquals(text, token.getText());
+		Scanner.LinePos l1 = token.getLinePos();
+		assertEquals(0,l1.line);
+		assertEquals(0,l1.posInLine);
+		//get the next token and check its kind, position, and contents
+		Scanner.Token token1 = scanner.nextToken();
+		assertEquals(Scanner.Kind.IDENT, token1.kind);
+		assertEquals(9, token1.pos);
+		text = token1.getText();
+		assertEquals(text.length(), token1.length);
+		assertEquals("def", token1.getText());
+		l1 = token1.getLinePos();
+		assertEquals(1,l1.line);
+		assertEquals(3,l1.posInLine);
+		/*
+		 * Scanner.Token token1 = scanner.nextToken();
+		assertEquals(Scanner.Kind.KW_IF, token1.kind);
+		assertEquals(6, token1.pos);
+		text = Scanner.Kind.KW_IF.getText();
+		assertEquals(text.length(), token1.length);
+		assertEquals("if", token1.getText());
+		*/
+		
+		//check that the scanner has inserted an EOF token at the end
+		Scanner.Token token3 = scanner.nextToken();
+		assertEquals(Scanner.Kind.EOF,token3.kind);
+	}
+
+
+@Test
+public void testwithprining() throws IllegalCharException, IllegalNumberException{
+	//String input = "/**? ]*/ */ hello\n   world+()&%! xloc-yloc|->";
+	//String input = "height /**/ abcdef /*abcd*/jkl/*";
+	//String input = "integer /**/ boolean integer | boolean | image | url | file | frame | while | if | sleep | screenheight | screenwidthgray | convolve | blur | scale gray /*abcd*/jkl";
+	//String input = "|->gray | convolve | blur | scale width | height xloc | yloc | hide | show | move true | false |  | &  |  ==  | !=  | < |  > | <= | >= | +  |  -  |  *   |  /   |  % | !  | -> |  |-> | <- | ;  | ,  |  (  |  )  | { | }";
+	//String input = "abc\tdk";
+	//String input = "this is a string literal\non two lines";
+	//String input = "00123integer abc\n123 789 99 hello if{a==b;}/**/";
+	//String input = "==!=";
+	//String input = "/*...*/a/***/\nbc!/ /*/ /**/ !\nd/*.**/";
+	String input = "|;|--->->-|->";
+	//System.out.println(input.length()+ " " + input);
+	Scanner scanner = new Scanner(input);
+	//thrown.expect(IllegalCharException.class);
+	scanner.scan();		
+	scanner.printall();
+	/*
+	 * Scanner.Token token1 = scanner.nextToken();
+	assertEquals(Scanner.Kind.KW_IF, token1.kind);
+	assertEquals(6, token1.pos);
+	text = Scanner.Kind.KW_IF.getText();
+	assertEquals(text.length(), token1.length);
+	assertEquals("if", token1.getText());
+	*/
+	
+	//check that the scanner has inserted an EOF token at the end
+	//Scanner.Token token3 = scanner.nextToken();
+	//assertEquals(Scanner.Kind.EOF,token3.kind);
+}
+}
